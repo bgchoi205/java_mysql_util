a63e7c2c41b8cfb43ee70696d38b4888b89fcfb4
bgchoi205 <choibg205@naver.com>
2021-06-17 23:23:24 +0900

    세팅

---
+++ src/com/cbg/mysqlutil/MysqlUtil.java
@@ -0,0 +1,5 @@
+package com.cbg.mysqlutil;
+
+public class MysqlUtil {
+
+}
---
+++ src/com/cbg/mysqlutil/Test.java
@@ -0,0 +1,12 @@
+package com.cbg.mysqlutil;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+
+	}
+
+}
